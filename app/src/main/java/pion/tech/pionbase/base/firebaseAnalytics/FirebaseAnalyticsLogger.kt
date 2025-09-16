6cdf5de74a1fe0053134e3ab3d7661702037c0b6
KhaiPV <khaipv@piontech.co>
2025-09-16 16:07:45 +0700

    refactor(analytics): improve parameter formatting in FirebaseAnalyticsLogger and enhance logScreen method to include screen class

--- app/src/main/java/pion/tech/pionbase/base/firebaseAnalytics/FirebaseAnalyticsLogger.kt
+++ app/src/main/java/pion/tech/pionbase/base/firebaseAnalytics/FirebaseAnalyticsLogger.kt
@@ -4,8 +4,22 @@ import android.os.Bundle
 
 interface FirebaseAnalyticsLogger {
     fun logEvent(eventName: String)
-    fun logEvent(eventName: String, bundle: Bundle)
-    fun logEvent(eventName: String, block: Bundle.() -> Unit)
-    fun logEvent(eventName: String, paramName: String, paramValue: String)
+
+    fun logEvent(
+        eventName: String,
+        bundle: Bundle,
+    )
+
+    fun logEvent(
+        eventName: String,
+        block: Bundle.() -> Unit,
+    )
+
+    fun logEvent(
+        eventName: String,
+        paramName: String,
+        paramValue: String,
+    )
+
     fun logScreen(screenName: String)
 }
--- app/src/main/java/pion/tech/pionbase/base/firebaseAnalytics/FirebaseAnalyticsLoggerImpl.kt
+++ app/src/main/java/pion/tech/pionbase/base/firebaseAnalytics/FirebaseAnalyticsLoggerImpl.kt
@@ -5,54 +5,67 @@ import com.google.firebase.analytics.FirebaseAnalytics
 import timber.log.Timber
 import javax.inject.Inject
 
-class FirebaseAnalyticsLoggerImpl @Inject constructor(
-    private val logger: FirebaseAnalytics
-) : FirebaseAnalyticsLogger {
-
-    companion object {
-        private const val TAG = "TESTERCHECKTRACKING"
-    }
-
-    override fun logEvent(eventName: String) {
-        Timber.tag(TAG).d("logEvent: $eventName")
-        logger.logEvent(eventName, Bundle.EMPTY)
-    }
-
-    override fun logEvent(eventName: String, bundle: Bundle) {
-        val stringBuilder = StringBuilder()
-        val keys: Set<String> = bundle.keySet()
-        for (key in keys) {
-            val value: Any? = bundle.get(key)
-            stringBuilder.append("($key : ${value.toString()}) ")
+class FirebaseAnalyticsLoggerImpl
+    @Inject
+    constructor(
+        private val logger: FirebaseAnalytics,
+    ) : FirebaseAnalyticsLogger {
+        companion object {
+            private const val TAG = "TESTERCHECKTRACKING"
         }
-        Timber.tag(TAG).d("logEvent: $eventName $stringBuilder")
-        logger.logEvent(eventName, bundle)
-    }
 
-    override fun logEvent(eventName: String, block: Bundle.() -> Unit) {
-        val bundle = Bundle().apply(block)
-        val stringBuilder = StringBuilder()
-        val keys: Set<String> = bundle.keySet()
-        for (key in keys) {
-            val value: Any? = bundle.get(key)
-            stringBuilder.append("($key : ${value.toString()}) ")
+        override fun logEvent(eventName: String) {
+            Timber.tag(TAG).d("logEvent: $eventName")
+            logger.logEvent(eventName, Bundle.EMPTY)
         }
-        Timber.tag(TAG).d("logEvent: $eventName $stringBuilder")
-        logger.logEvent(eventName, bundle)
-    }
 
-    override fun logEvent(eventName: String, paramName: String, paramValue: String) {
-        val bundle = Bundle()
-        bundle.putString(paramName, paramValue)
-        logger.logEvent(eventName, bundle)
-        Timber.tag(TAG).d("logEvent: $eventName ($paramName : $paramValue)")
-    }
-
-    override fun logScreen(screenName: String) {
-        val bundle = Bundle().apply {
-            putString(FirebaseAnalytics.Param.SCREEN_NAME, screenName)
+        override fun logEvent(
+            eventName: String,
+            bundle: Bundle,
+        ) {
+            val stringBuilder = StringBuilder()
+            val keys: Set<String> = bundle.keySet()
+            for (key in keys) {
+                val value: Any? = bundle.get(key)
+                stringBuilder.append("($key : $value) ")
+            }
+            Timber.tag(TAG).d("logEvent: $eventName $stringBuilder")
+            logger.logEvent(eventName, bundle)
         }
-        logger.logEvent(FirebaseAnalytics.Event.SCREEN_VIEW, bundle)
-        Timber.tag(TAG).d("logScreen: $screenName")
+
+        override fun logEvent(
+            eventName: String,
+            block: Bundle.() -> Unit,
+        ) {
+            val bundle = Bundle().apply(block)
+            val stringBuilder = StringBuilder()
+            val keys: Set<String> = bundle.keySet()
+            for (key in keys) {
+                val value: Any? = bundle.get(key)
+                stringBuilder.append("($key : $value) ")
+            }
+            Timber.tag(TAG).d("logEvent: $eventName $stringBuilder")
+            logger.logEvent(eventName, bundle)
+        }
+
+        override fun logEvent(
+            eventName: String,
+            paramName: String,
+            paramValue: String,
+        ) {
+            val bundle = Bundle()
+            bundle.putString(paramName, paramValue)
+            logger.logEvent(eventName, bundle)
+            Timber.tag(TAG).d("logEvent: $eventName ($paramName : $paramValue)")
+        }
+
+        override fun logScreen(screenName: String) {
+            val bundle =
+                Bundle().apply {
+                    putString(FirebaseAnalytics.Param.SCREEN_NAME, screenName)
+                    putString(FirebaseAnalytics.Param.SCREEN_CLASS, screenName)
+                }
+            logger.logEvent(FirebaseAnalytics.Event.SCREEN_VIEW, bundle)
+            Timber.tag(TAG).d("logScreen: $screenName")
+        }
     }
-}
