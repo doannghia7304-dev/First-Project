3b2d9749464c2c73b3d76277d9254ef4a0246527
MinhNN2381 <minhnm@piontech.co>
2026-07-27 12:07:00 +0700

    refactor(domain): update BaseUseCase structure and implementation classes

--- app/src/main/java/pion/tech/pionbase/domain/usecase/home/GetInstalledAppsUseCase.kt
+++ app/src/main/java/pion/tech/pionbase/domain/usecase/home/GetInstalledAppsUseCase.kt
@@ -1,12 +1,18 @@
 package pion.tech.pionbase.domain.usecase.home
 
+import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.flow.Flow
 import pion.tech.pionbase.data.model.installedApp.InstalledAppDtoModel
 import pion.tech.pionbase.data.repository.installedAppRepository.InstalledAppsRepository
+import pion.tech.pionbase.domain.usecase.base.BaseUseCase
 import pion.tech.pionbase.util.Result
 
 class GetInstalledAppsUseCase(
-    private val installedAppsRepository: InstalledAppsRepository
-) {
-    operator fun invoke(): Flow<Result<List<InstalledAppDtoModel>>> = installedAppsRepository.getInstalledApps()
+    private val installedAppsRepository: InstalledAppsRepository,
+) : BaseUseCase() {
+
+    operator fun invoke(): Flow<Result<List<InstalledAppDtoModel>>> =
+        executeFlow(dispatcher = Dispatchers.IO) {
+            installedAppsRepository.getInstalledApps()
+        }
 }
--- app/src/main/java/pion/tech/pionbase/domain/usecase/language/GetLanguagesUseCase.kt
+++ app/src/main/java/pion/tech/pionbase/domain/usecase/language/GetLanguagesUseCase.kt
@@ -1,12 +1,17 @@
 package pion.tech.pionbase.domain.usecase.language
 
+import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.flow.Flow
 import pion.tech.pionbase.data.model.language.LanguageDtoModel
 import pion.tech.pionbase.data.repository.languageRepository.LanguageRepository
+import pion.tech.pionbase.domain.usecase.base.BaseUseCase
 import pion.tech.pionbase.util.Result
 
 class GetLanguagesUseCase(
     private val languageRepository: LanguageRepository
-) {
-    operator fun invoke(): Flow<Result<List<LanguageDtoModel>>> = languageRepository.getLanguage()
+) : BaseUseCase() {
+
+    operator fun invoke(): Flow<Result<List<LanguageDtoModel>>> = executeFlow(dispatcher = Dispatchers.IO) {
+        languageRepository.getLanguage()
+    }
 }
